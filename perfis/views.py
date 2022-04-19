947e8f8c98bece3e01f33102a2a1e05259025a8c
Davi Luna <davi.luna@estudante.ifb.edu.br>
2022-04-19 14:42:49 -0300

    Modulo 4.1 - Primeiros passos com persistência feito com sucesso

--- perfis/views.py
+++ perfis/views.py
@@ -6,12 +6,6 @@ def index(request):
 
 def exibir(request, perfil_id):
 
-    perfil = Perfil()
-
-    if perfil_id == 1:
-        perfil = Perfil('Davi Luna', 'davi.luna@ifb.edu.br', '111111', 'IFB')
-    if perfil_id == 2:
-        perfil = Perfil('Marcos Rian', 'marcos.rian@ifb.edu.br', '40028922', 'IESB')
-
-    return render(request, 'perfil.html', {'perfil' : perfil})
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request, 'perfil.html', { "perfil" : perfil})
 # Create your views here.
