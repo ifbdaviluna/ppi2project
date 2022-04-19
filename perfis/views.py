05566fc1dd17efbaf95e944ebb1903e58893d58a
Davi Luna <davi.luna@estudante.ifb.edu.br>
2022-04-19 14:02:52 -0300

    Modulos 2 e 3 criados com sucesso

--- perfis/views.py
+++ perfis/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
+def index(request):
+    return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+
+    perfil = Perfil()
+
+    if perfil_id == 1:
+        perfil = Perfil('Davi Luna', 'davi.luna@ifb.edu.br', '111111', 'IFB')
+    if perfil_id == 2:
+        perfil = Perfil('Marcos Rian', 'marcos.rian@ifb.edu.br', '40028922', 'IESB')
+
+    return render(request, 'perfil.html', {'perfil' : perfil})
 # Create your views here.
