6d55364f326bf14cd048b5977bdda5fdaa00a772
Davi Luna <davi.luna@estudante.ifb.edu.br>
2022-05-03 12:10:20 -0300

    Módulo 6 completo e atualizado

--- perfis/views.py
+++ perfis/views.py
@@ -8,3 +8,13 @@ def exibir(request, perfil_id):
 	
 	perfil = Perfil.objects.get(id=perfil_id)
 	return render(request, 'perfil.html', {'perfil':perfil})
+
+def convidar(request, perfil_id):
+    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_a_convidar)
+
+    return redirect('index')
+
+def get_perfil_logado(request):
+    return Perfil.objects.get(id=1)
