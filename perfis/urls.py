489fb33143bd4c9c1960b8d560993ff3053a42da
Davi Luna <davi.luna@estudante.ifb.edu.br>
2022-05-24 11:50:34 -0300

    Modulo 8 atualizado

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from perfis import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
+	# re_path(r'^perfis/\d+$', views.exibir, name='perfis')
 	path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar')
 ]
--- perfis/views.py
+++ perfis/views.py
@@ -1,22 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from perfis.models import Perfil
-from django.shortcuts import redirect
 
 def index(request):
-	return render(request, 'index.html', {'perfis': Perfil.objects.all()})
+	return render(request, 'index.html', {'perfis': Perfil.objects.all(), 'perfil_logado': get_perfil_logado(request)})
 
-def exibir(request, perfil_id):
-	
+def exibir(request, perfil_id):	
 	perfil = Perfil.objects.get(id=perfil_id)
 	return render(request, 'perfil.html', {'perfil':perfil})
 
 def convidar(request, perfil_id):
-    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
-    perfil_logado = get_perfil_logado(request)
-    perfil_logado.convidar(perfil_a_convidar)
-
-    return redirect('index')
+	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+	perfil_logado = get_perfil_logado(request)
+	perfil_logado.convidar(perfil_a_convidar)
+	return redirect('index')
 
 def get_perfil_logado(request):
-    return Perfil.objects.get(id=1)
+	return Perfil.objects.get(id=1)
