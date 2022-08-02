4c3d80efc8b712a1b8d8abb5dcabb6269a10d27e
ifbdaviluna <davi.luna@estudante.ifb.edu.br>
2022-08-02 15:34:38 -0300

    Commitnado arquivos finais e prontos

--- perfis/urls.py
+++ perfis/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 	path('', include(router.urls)),
 	path('perfil/', views.get_meu_perfil, name='perfil'),
 	path('convites/', views.get_convites, name='convites'),
-	path('convite/convidar/<int:perfil_id>', views.convidar, name='convidar'),
-	path('convite/aceitar/<int:convite_id>', views.aceitar, name='aceitar'),
+	path('convites/convidar/<int:perfil_id>', views.convidar, name='convidar'),
+	path('convites/aceitar/<int:convite_id>', views.aceitar, name='aceitar'),
 	path('login/', auth_views.obtain_auth_token, name='login')
 ]
